5c03e2a424f1b479c5dff1de0ab4e6932cca8ee9
Dmitry <40324321+inilim@users.noreply.github.com>
2024-05-01 00:06:18 +0300

    ref

--- src/Str.php
+++ src/Str.php
@@ -4,6 +4,9 @@ namespace Inilim\String;
 
 use Inilim\LazyMethod\LazyMethodAbstract;
 
+/**
+ * @method string|false TranslatePhpUrlConstantToKey(int $constant)
+ */
 class Str extends LazyMethodAbstract
 {
     protected const NAMESPACE = 'Inilim\String\Method';
