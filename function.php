6f88c5327b21809fc0555210477c7fdc9420f1b7
Dmitry <40324321+inilim@users.noreply.github.com>
2024-05-30 23:05:16 +0300

    ref

--- function.php
+++ function.php
@@ -2,11 +2,10 @@
 
 use Inilim\String\Str;
 
-if (!function_exists('_str')) {
+if (!\function_exists('_str')) {
     function _str(): Str
     {
-        static $instance = null;
-        if ($instance !== null) return $instance;
-        return $instance = new Str;
+        static $o = new Str;
+        return $o;
     }
 }
