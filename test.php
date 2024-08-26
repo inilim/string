7f3e24171f2f0d80d7ae865bd44b32e136767269
Dmitry <inipel@ya.ru>
2024-08-26 17:10:59 +0300

    ref

--- test.php
+++ test.php
@@ -6,5 +6,8 @@ use Inilim\Dump\Dump;
 
 Dump::init();
 
+de(
+    str_replace('__SEPARATOR__', '|', \preg_quote(\_data()->URLProtocolsAsString('__SEPARATOR__')))
+);
 $a = _str()->startsWithUrlBeforePath('https://www.youtube.');
 dde($a);
