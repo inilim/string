4251971ef1c4a57265db41623018ed8bcf5be34b
Dmitry <inipel@ya.ru>
2024-08-26 18:38:36 +0300

    parseURL

--- src/Str.php
+++ src/Str.php
@@ -389,6 +389,36 @@ class Str
         return ASCII::is_ascii($value);
     }
 
+    /**
+     * new parse_url
+     * @return array{protocol:null|string,domain:null|string,port:null|int,login:null|string,password:null|string,path:null|string,query:null|string,anchor:null|string}
+     */
+    function parseURL(string $url): array
+    {
+        $r = \parse_url($url);
+        if (!\is_array($r)) $r = [];
+
+        $r['protocol'] = $r['scheme'] ?? null;
+        $r['domain']   = $r['host'] ?? null;
+        $r['anchor']   = $r['fragment'] ?? null;
+        $r['login']    = $r['user'] ?? null;
+        $r['password'] = $r['pass'] ?? null;
+
+        unset(
+            $r['host'],
+            $r['scheme'],
+            $r['fragment'],
+            $r['user'],
+            $r['pass']
+        );
+
+        $r['port'] ??= null;
+        $r['path'] ??= null;
+        $r['query'] ??= null;
+
+        return $r;
+    }
+
     function removeWWW(string $url): string
     {
         $res = \preg_replace('#^(www\.)#i', '', $url);
